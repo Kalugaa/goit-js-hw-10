fd933564fef7d6ad61f689c235b26d9f76c7bf2b
Kalugaa <133102595+Kalugaa@users.noreply.github.com>
2023-10-12 14:23:46 +0200

    1

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://api.thecatapi.com/v1';
+axios.defaults.headers.common['x-api-key'] =
+  'live_U9Wh061NmwnMWAhpBVlLlM7b4iGF1LWh1BsblD68mAsSJirrVfjpMXnHV3EXtlzH';
